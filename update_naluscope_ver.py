8a864ab70ff678a9c52e59cb9ca7337c46a46060
Marcus Luck <marcus.luck@outlook.com>
2024-09-13 14:38:53 -0700

    FIX: update current manual version

--- update_naluscope_ver.py
+++ update_naluscope_ver.py
@@ -32,27 +32,32 @@ def update_naluscope_version(new_version):
         yaml.dump(config, file, default_flow_style=False)
 
 
-def update_naluscope_manual_version(new_version):
+def update_naluscope_manual_version(new_version): 
     with open(BOARDS_FILE, 'r') as file:
         config = yaml.safe_load(file)
 
     for board in config.keys():
         if 'current' in config[board]:
-            # Move the NaluScope manual in current to the archived section
+            # Find the current NaluScope manual to archive
             current_manual = next((item for item in config[board]['current'] if 'Manual' in item['name']), None)
             if current_manual:
+                # Extract the board name using split on ".-." and ".User.Manual.pdf"
+                board_name_for_url = current_manual['url'].split('.-.')[1].split('.User.Manual.pdf')[0]
+
+                # Move the current NaluScope manual to the archived section
                 config[board]['archived'].insert(0, current_manual)
                 config[board]['current'].remove(current_manual)
 
             # Add a new entry for the updated NaluScope manual
             new_manual_entry = {
                 'name': f'NaluScope {new_version} Manual',
-                'url': f'https://github.com/NaluScientific/NaluScope-Release/releases/download/{new_version}/NaluScope.{new_version[1:]}.User.Manual.pdf'
+                'url': f'https://github.com/NaluScientific/NaluScope-Release/releases/download/{new_version}/NaluScope.{new_version[1:]}.-.{board_name_for_url}.User.Manual.pdf'
             }
 
             # Add the new manual to the current section
             config[board]['current'].insert(0, new_manual_entry)
 
+    # Write the updated configuration back to the file
     with open(BOARDS_FILE, 'w') as file:
         yaml.dump(config, file, default_flow_style=False)
 
@@ -68,3 +73,5 @@ if __name__ == '__main__':
         exit(1)
     update_naluscope_version(version)
     update_naluscope_manual_version(version)
+
+
